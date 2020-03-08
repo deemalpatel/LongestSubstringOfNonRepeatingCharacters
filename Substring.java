71d2eb316addac73075dc7050ebb96b196064b47
deemalpatel <dp55123@yahoo.com>
2020-03-07 22:34:48 -0800

    Update Substring.java

--- Substring.java
+++ Substring.java
@@ -7,27 +7,32 @@ public class Main {
 			System.out.print(lengthOfLongestSubstring("wasjcwr"));
 		}
 		public static int lengthOfLongestSubstring(String s) {
-	        int n = s.length(); // easier to just assign the length rather than calling it every time
-	        int startIndex = 0, slidingIndex = 0, counter = 0; // start Index, moving index, and counter
-		    Set<Character> set = new HashSet<>(); //Used HashSet so that there's no duplicates
-	        while(startIndex < n && slidingIndex < n){ //make the indices don't go past length of string
-	            if(set.contains(s.charAt(slidingIndex))){ //If we have the character already in the set
-	                set.remove(s.charAt(startIndex)); //remove that character then increment startIndex
-	                startIndex++;
-	            }
-	            
-	            /*
-	             * If it does not exist in set we add it in, then increment slidingIndex to the next value
-	             * to repeat the process. 
-	             * Increment counter to keep track of the longest substring
-	             */
-	            else{ 
-	                set.add(s.charAt(slidingIndex));
-	                slidingIndex++;
-	                counter = Math.max(counter, slidingIndex - startIndex); 
-	            }
-	        }
-	        return counter;
+        		if(s.isEmpty()) {
+			return 0;
+			}
+			Stack<Character>stck = new Stack<>();
+			int longest= 1;
+		
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (stck.contains(s.charAt(i))) 
+			{
+				while(stck.contains(s.charAt(i)))
+				{
+					stck.remove(0);
+				}
+				stck.push(s.charAt(i));
+	
+			}
+			else
+			{
+				stck.push(s.charAt(i));
+				longest = Math.max(stck.size(), longest);	
+			}
+		}
+		return Math.max(stck.size(), longest);
+		
+    }
 	    }
 		
 }
